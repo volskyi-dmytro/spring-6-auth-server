f59273fc215f37462f84e3d1974587a32d942640
@stpunk47 <stpunk47@gmail.com>
2025-02-13 21:41:22 +0200

    Initial commit

---
+++ src/main/java/com/stpunk47/spring_6_auth_server/Spring6AuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.stpunk47.spring_6_auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6AuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stpunk47/spring_6_auth_server/Spring6AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stpunk47.spring_6_auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
